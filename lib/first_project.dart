509e2d051eadec675bc4a821d4aedbf22bb3cd2b
Daniel Daniel <157515181+Danny00009@users.noreply.github.com>
2024-02-08 23:25:15 +0100

    Daniel's Project

---
+++ lib/first_project.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHommepage extends StatelessWidget {
+  const MyHommepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
